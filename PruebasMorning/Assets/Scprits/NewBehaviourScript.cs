0b993f72c6ebb1a1be45b5f4fa40703980859e38
Álvaro <correo@laultimapregunta.com>
2022-09-13 13:59:25 +0200

    Primera versión
    Diagrama de flujo de impacto

--- PruebasMorning/Assets/Scprits/NewBehaviourScript.cs
+++ PruebasMorning/Assets/Scprits/NewBehaviourScript.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    [SerializeField] int lifes;
+    [SerializeField] float energy;
+    float impact;
+    bool alive;
+    string message;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lifes = 3;
+        energy = 100f;
+        alive = true;
+
+        Impacto();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void Impacto()
     {
-        
+        impact = Random.Range(10f, 200f);
+
+        if(impact >= energy)
+        {
+            lifes--;
+
+            if(lifes <= 0)
+            {
+                message = "HAS MUERTO";
+                alive = false;
+            }
+            else
+            {
+                message = "Has perdido una vida. Te quedan " + lifes;
+                energy = 100f;
+            }
+
+        }
+        else
+        {
+            energy -= impact;
+        }
+
+        print(message);
     }
+
+
+
+
 }
